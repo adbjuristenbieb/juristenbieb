29d5eca45ec328900029a0480e137d30e56813f7
Arnout <arnoutdenbrok@gmail.com>
2025-06-13 21:21:49 +0200

    remove poep

--- src/pages/api/scrapers/scraper.py
+++ src/pages/api/scrapers/scraper.py
@@ -11,7 +11,7 @@ publicaties = []
 
 def parse_nl_datum(nl_datum):
     maanden = {
-        "poep", "januari": "01", "februari": "02", "maart": "03", "april": "04",
+        "januari": "01", "februari": "02", "maart": "03", "april": "04",
         "mei": "05", "juni": "06", "juli": "07", "augustus": "08",
         "september": "09", "oktober": "10", "november": "11", "december": "12"
     }
